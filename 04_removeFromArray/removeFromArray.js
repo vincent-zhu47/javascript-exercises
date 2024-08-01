3df67c4fcdf37f49bec560a75637d86d1fb5abd5
vincent-zhu47 <vincent.zhu@yale.edu>
2024-08-01 00:00:29 -0400

    Complete 4th javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    return arr.filter((item) => elements.indexOf(item) === -1);
 };
 
 // Do not edit below this line
