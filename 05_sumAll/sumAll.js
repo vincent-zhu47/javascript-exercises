9aa1074386ad00d58f9dad707ca4b521276757a3
vincent-zhu47 <vincent.zhu@yale.edu>
2024-08-01 00:06:44 -0400

    Complete 5th javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    return (num1+num2)*(Math.abs(num2-num1)+1)/2;
 };
 
 // Do not edit below this line
