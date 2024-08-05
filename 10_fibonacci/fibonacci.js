6e8efde2f687cd90711240379702134480b6fd20
vincent-zhu47 <vincent.zhu@yale.edu>
2024-08-04 23:45:15 -0400

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let curr = 1;
+    let prev = 0;
 
+    if (Number(index) === 0) {
+        return prev;
+    }
+    if (index < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 1; i < index; i++) {
+        let temp = curr;
+        curr+=prev;
+        prev = temp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
