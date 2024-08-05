1aef03e83c5adfb428027389079e3e6ec279389f
vincent-zhu47 <vincent.zhu@yale.edu>
2024-08-04 23:40:02 -0400

    Complete javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.replace(/[^0-9A-Z]+/gi, "").toLowerCase() === string.replace(/[^0-9A-Z]+/gi, "").split("").reverse().join("").toLowerCase();
 };
 
 // Do not edit below this line
