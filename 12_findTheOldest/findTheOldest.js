d87167b9674dabdcc4f227740e4c4fe4ace2b7b3
vincent-zhu47 <vincent.zhu@yale.edu>
2024-08-05 00:02:15 -0400

    Complete javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = 2024;
+    
+    return people.reduce((oldest, curr) => {
+        let oldestDeath;
+        let currDeath;
+        if (oldest.yearOfDeath === undefined) {
+            oldestDeath = currentYear;
+        } else {
+            oldestDeath = oldest.yearOfDeath;
+        }
+        if (curr.yearOfDeath === undefined) {
+            currDeath = currentYear;
+        } else {
+            currDeath = curr.yearOfDeath;
+        }
 
+        let oldestAge = oldestDeath - oldest.yearOfBirth;
+        let currAge = currDeath - curr.yearOfBirth;
+
+        return oldestAge > currAge ? oldest : curr;
+    });
 };
 
 // Do not edit below this line
